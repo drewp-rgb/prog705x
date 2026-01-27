68571a3133e7a578ac22a3de70ab46ba82c711be
drewp.rgb <drew.p@stu.janesville.k12.wi.us>
2026-01-27 09:17:10 -0600

    #

--- l705x.java
+++ l705x.java
@@ -7,5 +7,11 @@ public class l705x
     {
         Scanner input = new Scanner(new File("prog512h.txt"));
         Scanner input2 = new Scanner(new File("prog705x.txt"));
+        ArrayList<String> list = new ArrayList<>();
+        while(input2.hasNext())
+        {
+        list.add(input2.nextLine());
+        
+        }
     }
 }
