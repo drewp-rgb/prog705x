69e41636601632ac713d749398fb8bdb3d6aff51
drewp.rgb <drew.p@stu.janesville.k12.wi.us>
2026-01-27 09:36:53 -0600

    #

--- l705x.java
+++ l705x.java
@@ -1,17 +1,30 @@
 import java.util.*;
 import java.io.*;
 
-public class l705x
-{
-    public static void main(String [] args) throws IOException
-    {
+public class l705x {
+    public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(new File("prog512h.txt"));
         Scanner input2 = new Scanner(new File("prog705x.txt"));
         ArrayList<String> list = new ArrayList<>();
-        while(input2.hasNext())
+        ArrayList<String> word = new ArrayList<>();
+        while (input.hasNext()) {
+            String x = input.nextLine();
+            word.add(x.substring(0,5));
+            word.add(x.substring(5));
+        }
+
+        while (input2.hasNext()) {
+            list.add(input2.nextLine());
+
+        }
+
+        for(int lcv = 0;lcv < word.size();lcv++)
         {
-        list.add(input2.nextLine());
-        
+            int linepos = Integer.parseInt(word.get(lcv).substring(0,2));
+            int wordd = Integer.parseInt(word.get(lcv).substring(2,3));
+            int wordpos = Integer.parseInt(word.get(lcv).substring(3,4));
+            String[] words = list.get(linepos - 1).split(" ");
+            System.out.print(words[wordd-1].substring(wordpos-1, wordpos));
         }
     }
 }
