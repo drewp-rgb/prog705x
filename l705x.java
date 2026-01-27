b756ac2ef45e0f7f993acce9fb526f84a5e06b85
drewp.rgb <drew.p@stu.janesville.k12.wi.us>
2026-01-27 09:14:17 -0600

    test

--- l705x.java
+++ l705x.java
@@ -3,8 +3,9 @@ import java.io.*;
 
 public class l705x
 {
-    public static void main(String [] args)
+    public static void main(String [] args) throws IOException
     {
-
+        Scanner input = new Scanner(new File("prog512h.txt"));
+        Scanner input2 = new Scanner(new File("prog705x.txt"));
     }
 }
